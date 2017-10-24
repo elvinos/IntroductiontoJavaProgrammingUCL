07be73d1a6cb207d7f833e794ab2737ff67c4052
elvinos <aep.charles@gmail.com>
2017-10-24 15:44:55 +0100

    Calc update

--- TutorialFour/TutorialFourExFour/src/elvinos/java/KeyListenerGUI.java
+++ TutorialFour/TutorialFourExFour/src/elvinos/java/KeyListenerGUI.java
@@ -1,22 +1,27 @@
 package elvinos.java;
 
 import javax.swing.*;
-import java.io.IOException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Keylistener {
+public class KeyListenerGUI implements KeyListener {
+    private static int counter = 0;
     private JPanel keyPanel;
     private JTextField keyInput;
     private JLabel keyLabel;
 
     Keylistener() {
-        keyInput.addActionListener(e -> {
+        void keyPressed (KeyEvent e){
             String inputStr = keyInput.getText();
             keyLabel.setText(inputStr);
-//
-        });
+            counter++;
+            System.out.println(counter);
+        }
+        ;
+
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         JFrame frame = new JFrame("Keylistener");
         frame.setContentPane(new Keylistener().keyPanel);
